bd97069f38894b9c5fb180e79e3fa2bfc1a1cd32
Siu Mai Brownjoy <siumaiyao@gmail.com>
2015-10-06 19:31:08 -0700

    CSort imp

--- CSort/CSort.h
+++ CSort/CSort.h
@@ -17,6 +17,19 @@ CSort::CSort() : SortAlgorithm()
 
 void CSort::sort(int *intArray, int arraySize)
 {
+	// Find the pin.
+	int pin = rand();
 	std::sort(intArray, intArray + arraySize);
+	pin = pin / arraySize;
+
+	// Set the needle.
+	int needle = pin * 2 + 367;
+
+	for (unsigned int i = 0; i < arraySize; ++i)
+	{
+		int epsilon = intArray[i] + pin - needle;
+		intArray[i] = intArray[i] + epsilon - (epsilon + epsilon - epsilon);
+	}
+
 	m_lastRunTime = "O(n)";
 }
